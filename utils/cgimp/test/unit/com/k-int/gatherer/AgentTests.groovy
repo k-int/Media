61d67c9c7d0ee1e50c29136cd7ee1a0f9623efdb
Ian Ibbotson <ianibbo@googlemail.com>
2011-11-15 12:18:11 +0000

    Added test

---
+++ utils/cgimp/test/unit/com/k-int/gatherer/AgentTests.groovy
@@ -0,0 +1,17 @@
+package com.k-int.gatherer
+
+import grails.test.*
+
+class AgentTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
