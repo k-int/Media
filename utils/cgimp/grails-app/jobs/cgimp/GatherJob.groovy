e8dff65a487bb8f7902b08b9adbf7d17fbd1d1d0
Ian Ibbotson <ianibbo@googlemail.com>
2011-11-16 11:45:07 +0000

    Added quartz plugin

---
+++ utils/cgimp/grails-app/jobs/cgimp/GatherJob.groovy
@@ -0,0 +1,10 @@
+package cgimp
+
+
+class GatherJob {
+    def timeout = 5000l // execute job once in 5 seconds
+
+    def execute() {
+        // execute task
+    }
+}
---
+++ utils/cgimp/test/unit/cgimp/GatherJobTests.groovy
@@ -0,0 +1,17 @@
+package cgimp
+
+import grails.test.*
+
+class GatherJobTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
