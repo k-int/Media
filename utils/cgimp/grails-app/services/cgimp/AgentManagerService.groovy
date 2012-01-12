c6bc58ff8c491cfa0c1d45297a398c9d39921102
Ian Ibbotson <ianibbo@googlemail.com>
2012-01-12 13:39:01 +0000

    added missing service

---
+++ utils/cgimp/grails-app/services/cgimp/AgentManagerService.groovy
@@ -0,0 +1,47 @@
+package cgimp
+
+import org.springframework.core.io.Resource
+import org.codehaus.groovy.grails.commons.ApplicationAttributes
+import org.apache.shiro.crypto.hash.Sha256Hash
+import grails.util.GrailsUtil
+import com.k_int.gatherer.*
+
+
+class AgentManagerService {
+
+  def loadAgents(r) {
+    log.debug("Loading agents from disk cache");
+
+    def f = r.getFile();
+    log.debug("got agents dir: ${f}");
+
+    if ( f.isDirectory() ) {
+      f.listFiles().each { agent_file ->
+        log.debug("Procesing ${agent_file}");
+
+        GroovyClassLoader gcl = new GroovyClassLoader();
+        // GroovyClassLoader gcl = this.class.classLoader
+        Class clazz = gcl.parseClass(agent_file.text);
+        Object h = clazz.newInstance();
+
+        log.debug("Looking up agent ${h.getAgentName()}");
+        Agent agent = Agent.findByAgentName(h.getAgentName())
+        if ( agent == null ) {
+          agent = new Agent(agentName:h.getAgentName(),
+                            agentCode:agent_file.text,
+                            lastRun:null,
+                            nextDue:null,
+                            interval:86400)
+          if ( agent.save(flush:true) ) {
+            log.debug("Agent save ok");
+          }
+          else {
+            log.error("Problem saving agent ${agent.errors}");
+          }
+        }
+        log.debug("Loaded ${agent_file}");
+      }
+    }
+  }
+
+}
---
+++ utils/cgimp/test/unit/cgimp/AgentManagerServiceTests.groovy
@@ -0,0 +1,17 @@
+package cgimp
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(AgentManagerService)
+class AgentManagerServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
