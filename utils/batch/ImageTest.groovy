1f82a851c096989f393587f767ab4afdf68346f9
ianibo <ianibbo@googlemail.com>
2012-03-09 13:00:20 +0000

    Working custom exif tags

--- utils/batch/ImageTest.groovy
+++ utils/batch/ImageTest.groovy
@@ -10,21 +10,169 @@
   @Grab(group='org.apache.sanselan', module='sanselan', version='0.97-incubator')
 ])
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+  
+import org.apache.sanselan.ImageReadException;
+import org.apache.sanselan.ImageWriteException;
+import org.apache.sanselan.Sanselan;
+import org.apache.sanselan.common.IImageMetadata;
+import org.apache.sanselan.formats.jpeg.JpegImageMetadata;
+import org.apache.sanselan.formats.jpeg.exifRewrite.ExifRewriter;
+import org.apache.sanselan.formats.tiff.TiffField;
+import org.apache.sanselan.formats.tiff.TiffImageMetadata;
+import org.apache.sanselan.formats.tiff.constants.ExifTagConstants;
+import org.apache.sanselan.formats.tiff.constants.TagInfo;
+import org.apache.sanselan.formats.tiff.constants.TiffConstants;
+import org.apache.sanselan.formats.tiff.constants.TiffFieldTypeConstants;
+import org.apache.sanselan.formats.tiff.write.TiffOutputDirectory;
+import org.apache.sanselan.formats.tiff.write.TiffOutputField;
+import org.apache.sanselan.formats.tiff.write.TiffOutputSet;
+  
 def starttime = System.currentTimeMillis();
 def testuri = "http://media.culturegrid.org.uk/mediaLibrary/NCMG/BLDIDHH002634.jpg"
 
 println("Startup.. Initialising at ${starttime}");
 
-File image_dir = new File('images')
-if ( !image_dir.exists() ) {
-  image_dir.mkdirs();
-}
+// loadRemoteImage(testuri);
 
-File output_file = new File('images/file');
-def out = new BufferedOutputStream(new FileOutputStream(output_file));
+println("Add image history");
 
-out << new URL(testuri).openStream() 
-
-out.close()
+File f = new File('images/file');
+addImageHistoryTag(f);
 
 println("Completed after ${System.currentTimeMillis() - starttime}ms");
+
+
+def loadRemoteImage(uri) {
+
+  File image_dir = new File('images')
+  if ( !image_dir.exists() ) {
+    image_dir.mkdirs();
+  }
+
+  File output_file = new File('images/file');
+  def out = new BufferedOutputStream(new FileOutputStream(output_file));
+
+  out << new URL(testuri).openStream() 
+
+  out.close()
+}
+
+/**
+ * Example of adding an EXIF item to metadata, in this case using ImageHistory field. 
+ * (I have no idea if this is an appropriate use of ImageHistory, or not, just picked
+ * a field to update that looked like it wasn't commonly mucked with.)
+ * @param file
+*/
+def addImageHistoryTag(File file) {
+  File dst = null;
+  IImageMetadata metadata = null;
+  JpegImageMetadata jpegMetadata = null;
+  TiffImageMetadata exif = null;
+  OutputStream os = null;
+  TiffOutputSet outputSet = new TiffOutputSet();
+  
+  // establish metadata
+  try {
+      metadata = Sanselan.getMetadata(file);
+  } catch (ImageReadException e) {
+      e.printStackTrace();
+  } catch (IOException e) {
+      e.printStackTrace();
+  }
+  
+  // establish jpegMedatadata
+  if (metadata != null) {
+      jpegMetadata = (JpegImageMetadata) metadata;
+  }
+  
+  // establish exif
+  if (jpegMetadata != null) {
+      exif = jpegMetadata.getExif();
+  }
+  
+  // establish outputSet
+  if (exif != null) {
+      try {
+          outputSet = exif.getOutputSet();
+      } catch (ImageWriteException e) {
+          e.printStackTrace();
+      }
+  }
+  
+  if (outputSet != null) {         
+      // check if field already EXISTS - if so remove         
+
+      // add field 
+      try {  
+
+          String fieldData = "This is the image history tag value......"; 
+
+          TiffOutputDirectory exifDirectory = outputSet.getOrCreateExifDirectory();
+          addToDirectory(outputSet, TiffConstants.EXIF_TAG_XPCOMMENT,"MEDIA This is the image history tag value......", exifDirectory);
+          addToDirectory(outputSet, TiffConstants.EXIF_TAG_COPYRIGHT,"MEDIA copyright statement", exifDirectory);
+          addToDirectory(outputSet, TiffConstants.EXIF_TAG_IMAGE_ID ,"MEDIA image id", exifDirectory);
+          addToDirectory(outputSet, TiffConstants.EXIF_TAG_OWNER_NAME,"MEDIA Owner Name", exifDirectory);
+          addToDirectory(outputSet, TiffConstants.EXIF_TAG_SERIAL_NUMBER,"MEDIA Serial Number", exifDirectory);
+          // addToDirectory(outputSet, TiffConstants.EXIF_TAG_USER_COMMENT,"MEDIA User Comment", exifDirectory);
+          addToDirectory(outputSet, TiffConstants.EXIF_TAG_IMAGE_DESCRIPTION,"MEDIA Image Description", exifDirectory);
+          addToDirectory(outputSet, TiffConstants.EXIF_TAG_ARTIST,"MEDIA Artist == Creator", exifDirectory);
+          addToDirectory(outputSet, new TagInfo('CustomTag', 0xff11, TiffFieldTypeConstants.FIELD_TYPE_ASCII),"A Media Custom Tag... Flooflip", exifDirectory);
+
+      } catch (ImageWriteException e) {
+  
+          e.printStackTrace();
+      }
+  }
+   
+  try {
+      dst = new File("Hallo.jpg");
+      os = new FileOutputStream(dst);
+      os = new BufferedOutputStream(os);
+  } catch (IOException e) {
+      e.printStackTrace();
+  }
+  
+  // write/update EXIF metadata to output stream
+  try {
+      new ExifRewriter().updateExifMetadataLossless(file, os, outputSet);
+  } catch (ImageReadException e) {
+      e.printStackTrace();
+  } catch (ImageWriteException e) {
+      e.printStackTrace();
+  } catch (IOException e) {
+      e.printStackTrace();
+  } finally {
+    if (os != null) {
+      try {
+        os.close();
+      } 
+      catch (IOException e) {
+      }
+    }
+  }
+}
+   
+def addToDirectory(outputSet, tag, data, directory) {
+
+  // Remove any existing tag
+  TiffOutputField existing_tag = outputSet.findField(tag);
+  if (existing_tag != null) {
+    System.out.println("REMOVE ${tag}");
+    outputSet.removeField(tag);
+  }                    
+  TiffOutputField newField = new TiffOutputField(
+                    tag,
+                    // TiffFieldTypeConstants.FIELD_TYPE_BYTE,
+                    TiffFieldTypeConstants.FIELD_TYPE_ASCII,
+                    data.length(),
+                    data.getBytes());
+  // TiffOutputDirectory exifDirectory = outputSet.getOrCreateExifDirectory();
+  // exifDirectory.add(imageHistory);
+  directory.add(newField);
+}
