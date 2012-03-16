5fd37f408a981ece837e53d3477cf62064ef8aa5
ianibo <ianibbo@googlemail.com>
2012-03-16 16:00:29 +0000

    updated test steghide commmands

--- commands/steghide.sh
+++ commands/steghide.sh
@@ -2,13 +2,15 @@ cp input1.jpg input1b.jpg
 cp input2.png input2b.png
 
 echo hide id in input1b.jpg
-steghide embed -cf input1b.jpg -ef key.txt
+steghide embed -cf input1b.jpg -p "" -ef key.txt
 
 echo hide id in input2b.jpg
-steghide embed -cf input2b.png -ef key.txt
+steghide embed -cf input2b.png -p "" -ef key.txt
 
 echo Extracting form input1b.jpg
-steghide extract -sf input1b.jpg
+steghide extract -sf input1b.jpg -p "" -xf temp1
+cat temp1
 
 echo Extracting form input2b.jpg
-steghide extract -sf input2b.png
+steghide extract -sf input2b.png -p "" -xf temp2
+cat temp2
