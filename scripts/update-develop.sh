7156be4a553ceace4acd18f82b3fce510bb92705
Ian Ibbotson <ian.ibbotson@k-int.com>
2012-03-20 12:26:52 +0000

    Added update-dev

---
+++ scripts/update-develop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /home/media/apache-tomcat/bin
+# ./shutdown sh
+
+cd /home/media
+rm -Rf src
+mkdir src
+cd src
+git clone git@github.com:k-int/AggregatorCore.git
+git clone git@github.com:k-int/Media.git
+
+
+cd ~/src/AggregatorCore
+git checkout develop
+git pull
+cd ~/src/AggregatorCore/HandlerRegistry
+grails prod war
+cd ~/src/AggregatorCore/repository
+grails prod war
+
+cp ~/src/AggregatorCore/repository/target/repository-0.1.war ~/apache-tomcat/webapps/repository.war
+cp ~/src/AggregatorCore/HandlerRegistry/target/HandlerRegistry-0.1.war ~/apache-tomcat/webapps/HandlerRegistry.war
