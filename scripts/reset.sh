f078ebe018c35bfe8ffcda1fc46e4121d9df8c5b
Ian Ibbotson <ian.ibbotson@k-int.com>
2012-03-21 10:38:09 +0000

    updated reset script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,14 +3,20 @@
 
 # Clear down mongo
 
-# mongo <<!!!
-# show dbs
+mongo <<!!!
+show dbs
+use frbr
+db.dropDatabase()
+use media_rcs
+db.dropDatabase()
+use oda
+db.dropDatabase()
 # use xcri
 # show collections
 # db.aggregations.find({});
 # db.aggregations.remove({});
 # db.dropDatabase()
-# !!!
+!!!
 
 # Clear down ES indexes
 # curl -XDELETE 'http://localhost:9200/courses/course/_query?q=*:*'
