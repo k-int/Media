9693a4a62e0c55779e7f82c3309ae43893d91f1a
ianibo <ianibbo@googlemail.com>
2013-02-20 12:26:08 +0000

    Added extra deps for wsdl2java

--- research/soutronWS/gen.groovy
+++ research/soutronWS/gen.groovy
@@ -1,4 +1,14 @@
-@Grab(group='axis', module='axis', version='1.4')
+#!/usr/bin/groovy
+
+// @GrabResolver(name='es', root='https://oss.sonatype.org/content/repositories/releases')
+// @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
+@Grapes([
+  @Grab(group='axis', module='axis', version='1.4'),
+  @Grab(group='javax.activation', module='activation', version='1.1.1'),
+  @Grab( group='javax.mail', module='mail', version='1.4.4')
+])
+
+
 import org.apache.axis.wsdl.WSDL2Java
 class Generator {
 static def generate(){
