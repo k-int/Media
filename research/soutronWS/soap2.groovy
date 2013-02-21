4454ddee4175748a296fc9b649e33d851f6e1353
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-02-21 16:25:20 +0000

    added wslite client2

--- research/soutronWS/soap2.groovy
+++ research/soutronWS/soap2.groovy
@@ -4,20 +4,20 @@
 // @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
 @Grapes([
   // @Grab(group='axis', module='axis', version='1.4'),
-  @Grab(group='javax.activation', module='activation', version='1.1.1'),
-  @Grab( group='javax.mail', module='mail', version='1.4.4'),
-  @Grab(group='org.codehaus.groovy.modules', module='groovyws', version='0.5.2')
+  // @Grab(group='javax.activation', module='activation', version='1.1.1'),
+  // @Grab( group='javax.mail', module='mail', version='1.4.4')
+  @Grab(group='groovy', module='groovysoap-all-jsr06', version='0.1')
 ])
 
-import groovyx.net.ws.WSClient
+import groovy.net.soap.SoapClient
 
 println("Configure proxy...");
-ClassLoader cl=Thread.currentThread().getContextClassLoader() 
-def proxy = new WSClient("http://clientdemo.soutron.net/Library/WebServices/SoutronAPI.svc?wsdl",this.class.classLoader)
-// def proxy = new WSClient("http://clientdemo.soutron.net/Library/WebServices/SoutronAPI.svc?wsdl",cl);
+// ClassLoader cl=Thread.currentThread().getContextClassLoader() 
+// def proxy = new WSClient("http://clientdemo.soutron.net/Library/WebServices/SoutronAPI.svc?wsdl",this.class.classLoader)
+def proxy = new SoapClient("http://clientdemo.soutron.net/Library/WebServices/SoutronAPI.svc?wsdl")
 
 println("Initialize...");
-proxy.initialize()
+// proxy.initialize()
 
 println("search....");
 def result = proxy.SearchCatalogues("House", // String q
---
+++ research/soutronWS/soap3.groovy
@@ -0,0 +1,50 @@
+#!/usr/bin/groovy
+
+@Grapes([
+  @Grab(group='com.github.groovy-wslite', module='groovy-wslite', version='0.7.2')
+])
+
+import wslite.soap.*
+
+
+println("Configure proxy...");
+// def proxy = new SOAPClient("http://clientdemo.soutron.net/Library/WebServices/SoutronAPI.svc?wsdl")
+def proxy = new SOAPClient("http://clientdemo.soutron.net/Library/WebServices/SoutronAPI.svc/soap");
+
+println("search....");
+def result = proxy.send(SOAPAction: 'Soutron.SoutronGlobal.SoutronGlobalApi/ISoutronGlobalApi/SearchCatalogues') {
+  envelopeAttributes "xmlns:s": "Soutron.SoutronGlobal.SoutronGlobalApi"
+  body {
+    's:SearchCatalogues' {
+      q('House')
+      // searchId
+      // lang
+      // userId
+      // office
+      // opac
+      // standard
+      // page
+      // pageSize
+      // material
+      // ctrt
+      // fields
+      // sort
+      // usersearchId
+      // inclDepRec
+    }
+  }
+}
+
+
+println("Got result: ${result}");
+
+// def r2 = new String(result)
+// println("Result as string: ${r2}");
+
+println("Result class: ${result.dump}")
+println("Result: ${result.body}")
+byte[] bytes = result.body.text().getBytes()
+println(bytes);
+// response.httpResponse.statusCode == 200
+// response.soapVersion == SOAPVersion.V1_1
+
